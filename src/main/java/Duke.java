28f3f706b51b4e84977ea668d33a3b07dc285f0e
jelemii <xxjeremy2xx@gmail.com>
2024-03-29 16:17:06 +0800

    testing branch level 7 and filewriter object

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.DuplicateFormatFlagsException;
 import java.util.Scanner;
 
 public class Duke {
@@ -223,10 +225,22 @@ public class Duke {
             System.out.println((i+1) + ". " + taskList.get(i).toString());
         }
     }
+    private static void writeToFile(String filePath, String textToAdd) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(textToAdd);
+        fw.close();
+    }
     public static void main(String[] args) {
         String greetUser = "Hello! I'm jelemiiBot\n"
                 + "What can I do for you?\n";
         System.out.println(greetUser);
+
+        String file2 = "temp/lines.txt";
+        try {
+            writeToFile(file2, "first line" + System.lineSeparator() + "second line");
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
 
         boolean getInput = true;
         Scanner in = new Scanner(System.in);
